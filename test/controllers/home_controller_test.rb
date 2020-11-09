ec15d268428bd3e704fa466eeda6ebd0872617c6
ms02264 <ms02264@surrey.ac.uk>
2020-11-09 05:43:10 -0800

    Added Testing and Updated the Home page text

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'Cinema Town'
+    assert_select 'h1', 'Cinema Town'
+    assert_select 'p', 'Welcome to Cinema Booking website.'
+
   end
 
 end
